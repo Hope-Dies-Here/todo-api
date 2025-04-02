6d45b126fcc97fd5836a66c81712412239c5003d
hope-dies-here <onyourleftpal@gmail.com>
2025-04-02 21:33:40 +0300

    max length removed for password

--- models/User.js
+++ models/User.js
@@ -17,7 +17,6 @@ const userSchema = new mongoose.Schema({
 		type: String, 
 		required: [true, "Password is required"],
 		minlength: [6, "Password must be at least 6 characters long"],
-		maxlength: [32, "Password must be at most 32 characters long"], 
 	},
 })
 
